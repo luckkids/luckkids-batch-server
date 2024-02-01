8df52f780a7d96d0cf71af542279d62d5be51ab9
HOYA <als77770@gmail.com>
2024-02-01 17:30:49 +0900

    fix: 미션 푸시 배치 완료

--- mission_push/mysql_service.py
+++ mission_push/mysql_service.py
@@ -25,13 +25,12 @@ class MysqlService:
         except Error as e:
             raise Exception(f"Error while connecting to MySQL using Connection pool: {e}") from e
 
-    def get_mission_push(self, kst_date, kst_time):
+    def get_mission_push(self, kst_time, kst_date):
         with self.connection_pool.get_connection() as connection:
             with connection.cursor(dictionary=True) as cursor:
                 try:
-
                     sql = f"""
-                    SELECT m.description, m.alert_time, p.push_token 
+                    SELECT m.id, m.mission_description, m.alert_time, p.push_token 
                     FROM mission m 
                     JOIN push p ON m.user_id = p.user_id 
                     JOIN alert_setting a ON p.device_id = a.device_id AND a.mission = 'CHECKED' 
@@ -45,3 +44,15 @@ class MysqlService:
                     return result
                 except Error as e:
                     raise Exception(f"Error in MysqlService.get_mission_push: {e}") from e
+
+    def bulk_update_mission_push_date(self, idx_list, kst_date, batch_size=50):
+        with self.connection_pool.get_connection() as connection:
+            with connection.cursor() as cursor:
+                try:
+                    query = f"UPDATE mission SET push_date = '{kst_date}' WHERE id = %s"
+                    for i in range(0, len(idx_list), batch_size):
+                        batch = [(idx,) for idx in idx_list[i:i + batch_size]]
+                        cursor.executemany(query, batch)
+                        connection.commit()
+                except Error as e:
+                    raise Exception(f"Error in MysqlService.mark_as_deleted_bulk: {e}") from e
