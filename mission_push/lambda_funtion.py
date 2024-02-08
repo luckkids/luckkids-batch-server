aec9761cc20237447c1ce0f5a9cef38724072458
HOYA <102270909+mingeonho1@users.noreply.github.com>
2024-02-08 11:59:54 +0900

    fix: 미션 푸시 배치 완료 (#5)
    * fix: 미션 푸시 테스트중
    
    * Update .gitignore
    
    * fix: 미션 푸시 배치 완료
    
    * fix: 성공 slack은 주석처리 !
    
    5분마다 계속오면 너무 많이 쌓일 것 같아서 실패했을 때만 슬랙 받기

--- mission_push/lambda_funtion.py
+++ mission_push/lambda_funtion.py
@@ -6,33 +6,42 @@ from firebase_admin import credentials
 from firebase_admin import messaging
 import os
 import logging
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # load_dotenv()   # aws lambda에선 환경변수로
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+ID_LIST = []
+
 
 def lambda_handler(event, context):
     lambda_title = "mission_push_batch"
     try:
         slack = Slack(os.getenv("WEBHOOK_URL"), main_title=lambda_title)
-
         mysql = MysqlService(host=os.getenv("MYSQL_HOST"),
                              port=os.getenv("MYSQL_PORT"),
                              database=os.getenv("MYSQL_DATABASE"),
                              user=os.getenv("MYSQL_USER"),
                              password=os.getenv("MYSQL_PASSWORD"))
 
-        cred = credentials.Certificate(cert=os.getenv("FIREBASE_KEY_NAME"))
-        firebase_admin.initialize_app(cred)
+        try:
+            firebase_admin.get_app()
+        except ValueError:
+            cred = credentials.Certificate(cert=os.getenv("FIREBASE_KEY_NAME"))
+            firebase_admin.initialize_app(credential=cred)
 
-        result = mysql.get_mission_push(kst_date=Time.get_kst_today_string(), kst_time=Time.get_kst_time_string())
+        result = mysql.get_mission_push(kst_time=Time.get_kst_time_string(), kst_date=Time.get_kst_today_string())
 
-        for _ in map(send_push, result):
-            pass
+        for item in result:
+            send_push(item)
+            ID_LIST.append(item['id'])
 
+        mysql.bulk_update_mission_push_date(idx_list=ID_LIST, kst_date=Time.get_kst_today_string())
+
+        # success_message = slack.create_status_post(end_time=Time.get_kst_now())
+        # slack.post(success_message)
         success_response = create_response(status_code=200,
                                            body=f"{lambda_title} success!",
                                            end_time=Time.get_kst_now())
@@ -51,24 +60,28 @@ def lambda_handler(event, context):
 
 
 def send_push(item):
-    mission_description = item['description']
-    mission_alert_time = item['alert_time']
-    push_token = item['push_token']
+    try:
+        mission_description = item['mission_description']
+        mission_alert_time = item['alert_time']
+        push_token = item['push_token']
 
-    message = messaging.Message(
-        notification=messaging.Notification(
-            title='LUCK-KIDS 럭키즈🍀',
-            body=f"{mission_alert_time} '{mission_description}'로 행운을 +1 키워보아요!"
-        ),
-        token=push_token,
-    )
-    messaging.send(message)
+        message = messaging.Message(
+            notification=messaging.Notification(
+                title='LUCK-KIDS 럭키즈🍀',
+                body=f"{mission_alert_time} '{mission_description}'(으)로 행운을 +1 키워보아요!"
+            ),
+            token=push_token,
+        )
+        messaging.send(message)
 
-    logger.info({
-        'push_token': push_token,
-        'mission_description': mission_description,
-        'mission_alert_time': mission_alert_time
-    })
+        logger.info({
+            'push_token': push_token,
+            'mission_description': mission_description,
+            'mission_alert_time': str(mission_alert_time)
+        })
+
+    except Exception as e:
+        logger.error(f"Error in send_push: {e}")
 
 
 def create_response(status_code, body, end_time):
