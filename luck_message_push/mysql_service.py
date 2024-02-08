331bc074c162b1532578604c5466f2db2dab9533
HOYA <102270909+mingeonho1@users.noreply.github.com>
2024-02-08 12:00:05 +0900

    feat: 행운의 한마디 매일 7시마다 푸시 (#6)

---
+++ luck_message_push/mysql_service.py
@@ -0,0 +1,59 @@
+from mysql.connector import Error
+from mysql.connector import pooling
+
+
+class MysqlService:
+
+    def __init__(self, host, port, database, user, password, pool_name='mission_push_lambda', pool_size=5):
+
+        self.host = host
+        self.port = port
+        self.database = database
+        self.user = user
+        self.password = password
+
+        try:
+            self.connection_pool = pooling.MySQLConnectionPool(pool_name=pool_name,
+                                                               pool_size=pool_size,
+                                                               pool_reset_session=True,
+                                                               host=self.host,
+                                                               port=self.port,
+                                                               database=self.database,
+                                                               user=self.user,
+                                                               password=self.password)
+
+        except Error as e:
+            raise Exception(f"Error while connecting to MySQL using Connection pool: {e}") from e
+
+    def get_random_message(self):
+        with self.connection_pool.get_connection() as connection:
+            with connection.cursor(dictionary=True) as cursor:
+                try:
+                    sql = f"""
+                    SELECT message_description 
+                    FROM luck_message 
+                    ORDER BY RAND() 
+                    LIMIT 1;
+                    """
+                    cursor.execute(sql)
+                    result = cursor.fetchone()
+                    return result
+                except Error as e:
+                    raise Exception(f"Error in MysqlService.get_random_message: {e}") from e
+
+    def get_push_token(self):
+        with self.connection_pool.get_connection() as connection:
+            with connection.cursor(dictionary=True) as cursor:
+                try:
+                    sql = f"""
+                    SELECT push_token 
+                    FROM push p 
+                    JOIN users u ON u.id = p.user_id
+                    JOIN alert_setting als ON als.device_id = p.device_id
+                    WHERE als.luck_message = 'CHECKED';
+                    """
+                    cursor.execute(sql)
+                    result = cursor.fetchall()
+                    return result
+                except Error as e:
+                    raise Exception(f"Error in MysqlService.get_mission_push: {e}") from e
